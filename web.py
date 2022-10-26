531d06fc24dc2b5dda925de2cc322bfcceace5b3
dd <dd@dd.com>
2022-10-26 23:14:52 +0800

    second_commit

--- web.py
+++ web.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template
+from crypt import methods
+from flask import Flask, request, url_for, render_template, jsonify
 from pycaret.regression import *
 import pandas as pd
 import numpy as np
+import pickle
 
 app = Flask(__name__)
 
@@ -21,6 +23,14 @@ def predict():
     prediction = int(prediction.Label[0])
     return render_template("predict.html", pred=f"Expected Bill will be {prediction}")
 
+@app.route("/predict_api", methods=["POST"])
+def predict_api():
+    data = request.get_json(force=True)
+    input_data = pd.DataFrame([data])
+    prediction = predict_model(model, data=input_data)
+    output = prediction.Label[0]
+    return jsonify(output)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
